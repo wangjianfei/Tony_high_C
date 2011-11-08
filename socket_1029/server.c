935fb904d574204221e1f2c54dbaa2f1a95f260a
Tony_high_C <214653406@qq.com>
2011-11-08 09:48:31 +0800

    socket_1029

---
+++ socket_1029/server.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define SERV_PORT 8000
+
+int main(void)
+{
+    struct sockaddr_in servaddr,cliaddr;
+    socklen_t cliaddr_len;
+    int listenfd,connfd;
+    char buf[1024];
+    char str[1024];
+    int i ,n;
+
+    listenfd = socket(AF_INET,SOCK_STREAM,0);
+
+    bzero(&servaddr,sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htonl(INADDR_ANY);
+    servaddr.sin_addr.s_addr = htons(SERV_PORT);
+
+    bind(listenfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+
+    listen(listenfd,20);
+    printf("Accept connection ...\n");
+    while(1)
+    {
+        cliaddr_len = sizeof(cliaddr);
+        connfd = accept(listenfd,(struct sockaddr *)&cliaddr,&cliaddr_len);
+        n = read(connfd,buf,sizeof(buf));
+        printf("received from %s at PORT %d\n",inet_ntop(AF_INET,
+        &cliaddr.sin_addr,str,sizeof(str)),ntohs(cliaddr.sin_port));
+        for(i = 0;i < n;i++)
+        {
+            buf[i] = toupper(buf[i]);
+            write(connfd,buf,n);
+            close(connfd);
+        }
+    }
+    return 0;
+}
